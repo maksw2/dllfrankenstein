214ec328a9a91030c8b455db3e4848aa4ca2b895
Maksymilian Wilczynski <maksw@maksw.pl>
2026-02-06 22:52:11 +0100

    meaninglessly smarter build.bat

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 @echo off
 if not exist "obj" mkdir "obj"
+set CFILES=code\caller.cpp code\lexer.cpp code\parser.cpp
 set CFLAGS=/Ox /GL /Gw /Ob3 /Qpar /Fo"obj\\" /fp:fast /arch:AVX2 /std:c++latest /nologo /c /EHa /MP
 
-cl %CFLAGS% code\caller.cpp code\lexer.cpp code\parser.cpp || pause && exit /b
+cl %CFLAGS% %CFILES% || pause && exit /b
 ml64 /c /Fo"obj\\" /nologo code\helper.asm || pause && exit /b
 
 link obj\caller.obj obj\lexer.obj obj\parser.obj obj\helper.obj /LTCG /OPT:REF /OPT:ICF /nologo /out:invoke.exe || pause && exit /b
